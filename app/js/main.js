dd389ee362ae5a9bbf0d00ded96aa62f84d458ce
chnireva <danilaverin97@mail.ru>
2020-09-19 18:26:42 +0500

    added upload page

--- app/js/main.js
+++ app/js/main.js
@@ -35,6 +35,8 @@ $(function () {
     $('.icon-th-list').removeClass('active');
   });
 
+  $('input[type="file"], select').styler();
+
   $('.menu__btn').on('click', function () {
     $('.menu__list').slideToggle();
   });
